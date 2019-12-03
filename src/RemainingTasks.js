85ee41dfa4a5be85dee0d904c4f2b8c0cf442052
iggy-pigy <ilgakokorisa@gmail.com>
2019-12-03 12:52:18 +0000

    Created RemainingTasks.js file

---
+++ src/RemainingTasks.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class RemainingTasks extends React.Component{
+    render() {
+        return(
+            <p>Tasks to do : 4</p>
+        );
+    }
+}
+
+export default DeveloperCount;
