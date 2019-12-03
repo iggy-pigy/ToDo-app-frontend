967a942d710f351d7fc42226736d429a31320fe9
iggy-pigy <ilgakokorisa@gmail.com>
2019-12-03 12:53:35 +0000

    Imported RemainingTasks to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from "./Header";
+import RemainingTasks from "./RemainingTasks";
 
 
 import './App.css';
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header/>
+      <RemainingTasks/>
      
     </div>
   );
