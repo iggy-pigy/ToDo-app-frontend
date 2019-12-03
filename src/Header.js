ab7f3d48927726873d326676e45d5d167141715a
iggy-pigy <ilgakokorisa@gmail.com>
2019-12-03 12:37:17 +0000

    Added header.js file

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Header extends React.Component {
+    render() {
+        return(
+            <h1>Welcome to Iggy's ToDo application!</h1>
+        );
+    }
+}
+
+export default Header;
