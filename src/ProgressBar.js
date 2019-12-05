cef3e9077af8c051f070d1ddb7e5948bf1888789
iggy-pigy <ilgakokorisa@gmail.com>
2019-12-05 20:39:05 +0000

    Changed ProgressBar color

--- src/ProgressBar.js
+++ src/ProgressBar.js
@@ -5,15 +5,14 @@ import styled from "styled-components";
 const Track = styled.div`
 width: 100%;
 height: 20px;
-background-color: #222;
+background-color: #FFB5FC;
 border-radius: 10px;
-box-shadow: inset 0 0 5px #000;
 `
 
 const ProgressInTracker = styled.div`
 width: ${props => props.percentage}%;
 height: 100%;
-background-color: #6bccf9;
+background-color: #007BFF;
 border-radius: 8px;
 `
 
