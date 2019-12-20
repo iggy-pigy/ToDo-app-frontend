21841a2dd9294a35f77a63ff16fe7c4512b4bc75
iggy-pigy <ilgakokorisa@gmail.com>
2019-12-20 16:26:03 +0000

    Added onClick  event attribute to Delete button

--- src/Gherkin.js
+++ src/Gherkin.js
@@ -1,17 +1,19 @@
-import React from "react"
+import React from "react";
 import "./Gherkin.css";
 import gherkin from "./docs/giphy.gif";
 
-
 class Gherkin extends React.Component {
-    render() {
-        return (
-            <div className="Gherkin">
-                <img src={gherkin} alt="Dancing-gherkin" style={{ width: 150, height: 150 }} />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="Gherkin">
+        <img
+          src={gherkin}
+          alt="Dancing-gherkin"
+          style={{ width: 150, height: 150 }}
+        />
+      </div>
+    );
+  }
 }
-
 
 export default Gherkin;
--- src/Tasks.js
+++ src/Tasks.js
@@ -1,23 +1,29 @@
-import React from "react"
+import React from "react";
 import "./Tasks.css";
 
-
-
 class Tasks extends React.Component {
-    render() {
-        return (
-            <div className="row">
-                <div className="col-6">
-                    <p>{this.props.task}</p>
-                </div>
-                <div className="col-6">
-                    {this.props.done === true ? <button className="button">Delete</button> :
-                    <button className="button">Done</button>
-                    }
-                </div>
-            </div>
-        );
-    }
+  handleDelete = () => {
+    this.props.deleteTaskFunc(this.props.id);
+  };
+
+  render() {
+    return (
+      <div className="row">
+        <div className="col-6">
+          <p>{this.props.task}</p>
+        </div>
+        <div className="col-6">
+          {this.props.done === true ? (
+            <button className="button" onClick={this.handleDelete}>
+              Delete
+            </button>
+          ) : (
+            <button className="button">Done</button>
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Tasks;
