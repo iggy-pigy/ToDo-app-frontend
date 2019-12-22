e00b3d4fb38fbe95793a42c7f427256fe7df92aa
iggy-pigy <ilgakokorisa@gmail.com>
2019-12-22 17:14:07 +0000

    Tried to add progress label to ProgressBar

--- src/ProgressBar.js
+++ src/ProgressBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import "./ProgressBar.css";
 
 const Track = styled.div`
   max-width: 70%;
@@ -8,6 +9,7 @@ const Track = styled.div`
   background-color: #ffb5fc;
   border-radius: 10px;
   margin: 0px 10px 80px 10px;
+  text-align: center;
 `;
 
 const ProgressInTracker = styled.div`
@@ -15,13 +17,13 @@ const ProgressInTracker = styled.div`
   height: 100%;
   background-color: #007bff;
   border-radius: 8px;
+  text-align: center;
 `;
 
 class ProgressBar extends React.Component {
   render() {
     return (
       <div>
-        <h4>Progress</h4>
         <Track className="track">
           <ProgressInTracker percentage={this.props.percentage} />
         </Track>
