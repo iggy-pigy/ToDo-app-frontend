b3484cf46a27a7b4c55689faafa44861ce56692b
iggy-pigy <ilgakokorisa@gmail.com>
2020-01-18 21:03:01 +0000

    Updates

---
+++ precache-manifest.ad585c6b08aadf7e83ba66d529aaade9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7162b614c9b8b1178325429f34379b3d",
+    "url": "/ToDo-app-frontend/index.html"
+  },
+  {
+    "revision": "c8eb82092f5daaedee9a",
+    "url": "/ToDo-app-frontend/static/css/main.3c4cd8ed.chunk.css"
+  },
+  {
+    "revision": "de0ed500ad348406f102",
+    "url": "/ToDo-app-frontend/static/js/2.34972d37.chunk.js"
+  },
+  {
+    "revision": "c8eb82092f5daaedee9a",
+    "url": "/ToDo-app-frontend/static/js/main.b1e12def.chunk.js"
+  },
+  {
+    "revision": "e18a4bc007b3fc8f1a65",
+    "url": "/ToDo-app-frontend/static/js/runtime-main.4a86bf0f.js"
+  },
+  {
+    "revision": "8b190cbb170d50a2543af06daaed3444",
+    "url": "/ToDo-app-frontend/static/media/cucamber_6.8b190cbb.jpg"
+  },
+  {
+    "revision": "d586e3dc5b8c8710fa4bc49c4bd433dd",
+    "url": "/ToDo-app-frontend/static/media/giphy.d586e3dc.gif"
+  }
+]);
